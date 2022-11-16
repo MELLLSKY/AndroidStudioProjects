fbd336da5a69ea21e1b300c210401e4e235b7519
MELLLSKY <meliseky@gmail.com>
2022-11-16 12:58:07 +0300

    all weeks commit

--- MyBrowser/app/src/main/java/com/example/mybrowser/MainActivity.java
+++ MyBrowser/app/src/main/java/com/example/mybrowser/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.mybrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnGo;
+    EditText txtAddress;
+    WebView webView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTest =findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mu.edu.tr/tr"));
+            }
+        });
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl("http://"+ txtAddress.getText());
+            }
+        });
+        btnGo = findViewById(R.id.go);
+        txtAddress = findViewById(R.id.adress);
+        webView = findViewById(R.id.webview);
+
+
+
+        if (getIntent() !=null && getIntent().getData()!=null){
+            txtAddress.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
